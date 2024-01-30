a849ae45947a3dc7f97b23f0268952067195d294
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-30 16:10:23 +0100

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/Counter";
+import ClickablePicture from "./components/ClickablePicture";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <LikeButton />
+      <Counter />
+      <ClickablePicture />
     </div>
   );
 }
