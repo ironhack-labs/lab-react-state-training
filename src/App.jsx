28c790b0f96b04e19a5eb6b1ecdbb062368e931d
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-30 22:02:01 +0100

    solve iteration 5 DiscoButton

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import ClickablePicture from "./components/ClickablePicture";
 import Counter from "./components/Counter";
 import Dice from "./components/Dice";
 import LikeButton from "./components/LikeButton";
+import DiscoButton from "./components/DiscoButton";
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <h1> LAB | React State Training</h1>
+      <h2>Iteration 1: LikeButton</h2>
       <LikeButton />
+      <h2>Iteration 2: Counter</h2>
       <Counter />
-      <ClickablePicture/ >
+      <h2>Iteration 3: ClickablePicture</h2>
+      <ClickablePicture />
+      <h2>Iteration 4: Dice</h2>
       <Dice />
+      <h2>Iteration 5: DiscoButton</h2>
+      <DiscoButton />
     </div>
   );
 }
