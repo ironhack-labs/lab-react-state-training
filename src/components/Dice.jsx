42b1144e450a123345c03571e375d8a997218334
Alastair Longmuir <alastairlinwood@gmail.com>
2024-02-05 22:47:48 +0100

    fixed setTimeout

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -5,29 +5,15 @@ function Dice () {
     const [dice, newDice] = useState('./src/assets/images/dice3.png')
 
     const rollDice = () => {
-        setTimeout(newDice('./src/assets/images/dice-empty.png'),1000);
-        const randomNum = Math.floor(Math.random() * 6 + 1)
+        newDice('./src/assets/images/dice-empty.png')
 
-        switch (randomNum) {
-            case 1:
-                setTimeout(newDice('./src/assets/images/dice1.png'), 1000);
-                break;
-            case 2:
-                setTimeout(newDice('./src/assets/images/dice2.png'), 1000);
-                break;
-            case 3:
-                setTimeout(newDice('./src/assets/images/dice3.png'), 1000);
-                break;
-            case 4:
-                setTimeout(newDice('./src/assets/images/dice4.png'), 1000);
-                break;
-            case 5:
-                setTimeout(newDice('./src/assets/images/dice5.png'), 1000);
-                break;
-            case 6:
-                setTimeout(newDice('./src/assets/images/dice6.png'), 1000);
-                break;
-        }
+setTimeout(() => {
+
+    const randomNum = Math.floor(Math.random() * 6 + 1)
+    newDice(`.src/assets/images/dice${randomNum}.png`)
+
+}, 1000)
+
 
 
     }
