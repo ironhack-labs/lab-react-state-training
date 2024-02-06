3e00aa434c985db694af146d224f3953d5c1ecc3
Marianna Di Vito <maridivi89@gmail.com>
2024-02-06 19:26:21 +0000

    Add like button and more style

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import Carousel from "./components/Carousel";
 import ClickablePicture from "./components/ClickablePicture";
 import Counter from "./components/Counter";
 import Dice from "./components/Dice";
+import DiscoButton from "./components/DiscoButton";
 import LikeButton from "./components/LikeButton";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <h1> LikeButton</h1>
+
       <LikeButton />
+      <h1> Counter</h1>
       <Counter />
+      <h1>ClickablePicture</h1>
       <ClickablePicture />
+      <h1> Dice</h1>
+
       <Dice />
+      <h1>DiscoButton</h1>
+      <DiscoButton />
+      <h1> Carousel</h1>
       <Carousel
         images={[
           "https://randomuser.me/api/portraits/women/1.jpg",
--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,20 +1,9 @@
 import { useState } from "react";
 
 export default function LikeButton() {
-  const bgColors = ["purple", "blue", "green", "yellow", "orange", "red"];
-
   const [likesCount, setLikesCount] = useState(0);
-  const [buttonColorIndex, setButtonColorIndex] = useState(0);
   function incrementCount() {
     setLikesCount(likesCount + 1);
-    setButtonColorIndex((prevIndex) => (prevIndex >= 5 ? 0 : prevIndex + 1));
   }
-  return (
-    <button
-      style={{ backgroundColor: `${bgColors[buttonColorIndex]}` }}
-      onClick={incrementCount}
-    >
-      {likesCount} Likes
-    </button>
-  );
+  return <button onClick={incrementCount}>{likesCount} Likes</button>;
 }
