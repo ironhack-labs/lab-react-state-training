d768afe157b4a1b3960988710411cead975b73f8
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-02-06 16:25:34 +0100

    managed iteration 3

--- src/components/ClickablePicture.jsx
+++ src/components/ClickablePicture.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
+import defaultImage from '../assets/images/maxence.png';
+import newImage from '../assets/images/maxence-glasses.png';
+
 function ClickablePicture () {
+
+    const [defaultImg, setNewImg] = useState(defaultImage)
+
+    function changePicture () {
+        if  (defaultImg === defaultImage){
+        setNewImg(newImage)
+    }
+        else if (defaultImg === newImage) {
+        setNewImg(defaultImage)
+    }      
+}
+ 
+
     return (
         <div>
-
+            <img src={defaultImg} onClick={changePicture}/>
         </div>
     )
 }
