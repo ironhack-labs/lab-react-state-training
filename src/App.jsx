6a2f5cc87fc3e3b5048b6c2bb891002b385bc0c4
arisar86 <ari.sariannidis@gmail.com>
2024-02-06 15:37:44 +0100

    iteration 3 solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import LikeButton from "./components/LikeButton";
 import Counter from "./components/Counter";
+import ClickablePicture from "./components/ClickablePicture";
 
 function App() {
   return (
     <div className="App">
       <LikeButton />
       <Counter />
+      <ClickablePicture />
     </div>
   );
 }
