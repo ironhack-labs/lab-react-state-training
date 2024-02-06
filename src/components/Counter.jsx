01e6d7e3fe92dd7907892fc3d1a35385d4f92f33
Daniel_Costa <dan.costaa.12@gmail.com>
2024-02-06 19:50:03 +0000

    commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,11 +5,11 @@ function Counter() {
     const [count, setCount] = useState(0);
     return (
         
-      <section style={{display:"flex"}}>
+      <section style={{display:"flex", flexDirection:"row"}}> 
         <article>
-        <button onClick = {()=>setCount(count + 1)}>+</button>
+        <button style={{background: `rgb(46,108,204)`}} onClick = {()=>setCount(count + 1)}>+</button>
         <p>{count}</p>
-        <button onClick = {()=>count>0 ? setCount(count - 1): setCount (0)}>-</button>
+        <button style = {{background: `rgb(46,108,204)`}} onClick = {()=>count>0 ? setCount(count - 1): setCount (0)}>-</button>
         </article>
       </section>
     );
