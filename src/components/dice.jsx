1cd632752cd92bc28609655cdca7b76038ef3c57
Haripriya <haripriyagct@gmail.com>
2024-02-06 23:39:33 +0100

    second commit

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 function Dice(){
-    let [imageSrc, setImageSrc] = useState('./src/assets/images/dice-empty.png');
-    const imageArray = ['./src/assets/images/dice-empty.png','./src/assets/images/dice1.png','./src/assets/images/dice2.png',
+    let [imageSrc, setImageSrc] = useState('./src/assets/images/dice4.png');
+    const imageArray = ['./src/assets/images/dice1.png','./src/assets/images/dice2.png',
     './src/assets/images/dice3.png','./src/assets/images/dice4.png','./src/assets/images/dice5.png',
     './src/assets/images/dice6.png']
     function changeImage(){
         let randomImg = imageArray[Math.floor(Math.random() * (imageArray.length))] 
-        setImageSrc(randomImg);
+        setImageSrc('./src/assets/images/dice-empty.png');
+        setTimeout(()=>{
+            setImageSrc(randomImg);
+        },1000)
+       
     }
 return(<>
 <img src={imageSrc} onClick={changeImage} />
