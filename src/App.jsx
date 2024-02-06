a11d87d80f7d19888ce52118db52cb212e7e0dbe
Alina Havriushenko <alinagvr@gmail.com>
2024-02-06 15:28:22 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import ClickablePicture from "./components/ClickablePicture";
+import Counter from "./components/Counter";
+import LikeButton from "./components/LikeButton";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <LikeButton />
+      <br />
+      <Counter />
+      <ClickablePicture />
     </div>
   );
 }
