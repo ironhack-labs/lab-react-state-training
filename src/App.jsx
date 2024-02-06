7e7c1eeb34a00d23da4906be9ab60c05f84df39a
pouria pou <pouriap024@yahoo.com>
2024-02-06 14:58:19 +0100

    iteration 2 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import LikeBtn from "./components/LikeBtn";
+import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
       <LikeBtn />
+      <Counter />
     </div>
   );
 }
