bed42e1d56d2992f6d3353abf5e5e7e283d96ac2
Laia Navalon <laianavalon@gmail.com>
2024-02-06 16:02:07 +0100

    Iteration 0-2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+
+      <LikeButton />
+      <Counter />
     </div>
   );
 }
