0f83808c92867b666b5f0996ba008d76488c6494
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-07 01:38:26 +0100

    Remove existing code and adjust it to the requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+
+import LikeButton from "./components/LikeButton";
+
+
 
 function App() {
   return (
-    <div className="App">
+
+    <>
+      <div className="App">
       <h1> LAB | React Training</h1>
     </div>
+    <LikeButton />
+    </>
+  
+
   );
 }
 
