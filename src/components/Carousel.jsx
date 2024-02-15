da558c50fd1848605d37a24fde66992edefc5a85
pablosanchezcamara89@gmail.com <pablosanchez@MacBook-Air-de-Pablo.local>
2024-02-15 09:25:17 +0100

    Cambios buenos

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function Carousel(props) {
+  const [currentImgCarousel, setCurrentImgCarousel] = useState(props.images[0]);
+  const handleLeftMoveImg = () => {
+    setCurrentImgCarousel(currentImgCarousel - 1);
+  };
 
-    const [currentImgCarousel, setCurrentImgCarousel] = useState(props.images[0])
-    const handleLeftMoveImg = () => {
-        setCurrentImgCarousel (currentImgCarousel - 1)
-    }
-
-    const handleRightMoveImg = () => {
-        setCurrentImgCarousel (currentImgCarousel + 1)
-    }
+  const handleRightMoveImg = () => {
+    setCurrentImgCarousel(currentImgCarousel + 1);
+  };
 
   return (
     <div>
-        <button onClick={handleLeftMoveImg}>Left</button>
-        <img src={currentImgCarousel} alt="img" width="200px" />
-        <button onClick={handleRightMoveImg}>Right</button>
+      <button onClick={handleLeftMoveImg}>Left</button>
+      <img src={currentImgCarousel} alt="img" width="200px" />
+      <button onClick={handleRightMoveImg}>Right</button>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -6,16 +6,19 @@ import dice3 from "../assets/images/dice3.png"
 import dice4 from "../assets/images/dice4.png"
 import dice5 from "../assets/images/dice5.png"
 import dice6 from "../assets/images/dice6.png"
+const diceArr = [dice1, dice2, dice3, dice4, dice5, dice6];
 
 function Dice() {
-    const diceArr = [dice1, dice2, dice3, dice4, dice5, dice6];
     const [currentDiceImg, setCurrentDiceImg] = useState(diceEmpty)
     
-    
     const handleRandomDice = () => {
-      const randomDiceIndex = Math.floor(Math.random() * diceArr.length)
-      const randomDice = diceArr[randomDiceIndex]
-      setCurrentDiceImg (randomDice)
+      setCurrentDiceImg(diceEmpty)
+      setTimeout(() => {
+        const randomDiceIndex = Math.floor(Math.random() * diceArr.length)
+        const randomDice = diceArr[randomDiceIndex]
+
+        setCurrentDiceImg (randomDice)
+      }, 1000)
     }
 
     //setinterval
