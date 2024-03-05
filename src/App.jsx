7aca087774a43ef21d88eecf639972883593bd6f
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-05 14:19:17 +0100

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { ClickablePicture } from "./components/ClickablePicture";
 import { Counter } from "./components/Counter";
 import { LikeButton } from "./components/LikeButton";
 
 function App() {
   return (
     <>
+      <ClickablePicture />
       <Counter />
       <LikeButton />
     </>
