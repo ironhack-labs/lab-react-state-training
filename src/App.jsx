76c64833660575e25b5fbc659e8f174c1d9d9711
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-05 15:03:57 +0100

    Solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ClickablePicture } from "./components/ClickablePicture";
 import { Counter } from "./components/Counter";
+import { Dice } from "./components/Dice";
 import { LikeButton } from "./components/LikeButton";
 
 function App() {
   return (
     <>
+      <Dice />
       <ClickablePicture />
       <Counter />
       <LikeButton />
