6d33e264ebe0b4fd02fa82e8b02c015dff988cb4
onurcangulpak <onurcangulpak@gmail.com>
2024-03-13 17:12:56 +0100

    done

---
+++ src/components/ClickablePicture.jsx
@@ -0,0 +1,19 @@
+import "./ClickablePicture.css";
+import img from "../assets/images/maxence.png";
+import imgGlasses from "../assets/images/maxence-glasses.png";
+
+import { useState } from "react";
+
+export const ClickablePicture = () => {
+  const [imgState, setImgState] = useState(false);
+
+  const changeImg = () => {
+    setImgState(!imgState);
+  };
+
+  return imgState == false ? (
+    <img onClick={changeImg} src={img} alt="Maxence" />
+  ) : (
+    <img onClick={changeImg} src={imgGlasses} alt="Maxence Glasses" />
+  );
+};  
